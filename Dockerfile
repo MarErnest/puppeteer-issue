b2edb25849ef555839d90c4f323902c1af358fe8
Mark Ernest R. Matute <markernest.matute@gmail.com>
2022-06-29 20:45:10 +0800

    attempt-1

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,11 @@ WORKDIR /app
 
 RUN apt-get update && apt-get install -y procps && rm -rf /var/lib/apt/lists/*
 
+RUN apt-get update \
+  && apt-get install -y chromium \
+  fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-kacst fonts-freefont-ttf libxss1 \
+  --no-install-recommends
+
 RUN npm install -g yarn --force
 
 RUN npm config set registry http://registry.npmjs.org
@@ -11,6 +16,10 @@ RUN npm config set registry http://registry.npmjs.org
 ADD package.json /app/package.json
 
 ADD yarn.lock /app/yarn.lock
+
+ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD true
+
+ENV PUPPETEER_EXECUTABLE_PATH /usr/bin/chromium
 
 RUN yarn
 
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,25 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import puppeteer from 'puppeteer';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
   @Get()
-  getHello() {
+  async getHello() {
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox'],
+    });
+    const page = await browser.newPage();
+    await page.goto('https://google.com');
+    await page.screenshot({ path: 'google.png' });
+
+    await browser.close();
+
     return {
-      ako: 'nalulula',
+      just: 'did a google screenshot',
     };
   }
 }
